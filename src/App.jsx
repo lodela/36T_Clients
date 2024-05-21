b5ab9bd0105ad7ba66aee5f7d5e375f2cd2db0a2
lodela <lodela.n@gmail.com>
2024-05-21 00:24:06 -0600

    Updated files and acceptance criteria

--- src/App.jsx
+++ src/App.jsx
@@ -36,9 +36,25 @@ cualquier otra alternativa )
 
 /** 
  * 3.- Creacion de la tabla.
- * Hacer una pantalla con la siguiente url: /clientes, donde se muestre una tabla mostrando la información de los clientes, 
- * Poner un botón para eliminar los registros y uno para editarlos
+ * ✅ Hacer una pantalla con la siguiente url: /clientes, donde se muestre una tabla mostrando la información de los clientes, 
+ * ✅ Poner un botón para eliminar los registros y 
+ * ✅ Poner un botón para editarlos
  * Se tiene que agregar un cuadro de búsqueda en la tabla para filtrar por nombre comercial 
  * A continuación se muestra una idea de lo que se busca ( no tiene que ser igual ) 
 
 */
+/**
+ * 4.- Hacer una pantalla con la siguiente url: /nuevo-cliente, donde se muestre un formulario con los campos del cliente, 
+Se tiene que validar lo siguiente:
+* Que el correo sea válido 
+* Nombre comercial no sea cadena vacía o nulo
+
+ */
+
+/**
+ * Hacer una pantalla con la siguiente url: /cliente/:id, donde se muestre un formulario para poder editar el cliente que viene en :id, ( se tiene que precargar la información del cliente ). 
+ * Se tiene que validar lo siguiente: 
+ * que el correo sea válido , 
+ * nombre comercial no sea una cadena vacía
+
+ */
